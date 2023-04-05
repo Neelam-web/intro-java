a3468279ab69c2195be8e972cd812347c07267e6
neelam-web <neelamfatimah777@gmail.com>
2023-04-05 10:35:50 -0700

    INTRO JAVA

---
+++ scripts/add.js
@@ -0,0 +1,145 @@
+alert ('hello js')
+var xy=prompt("Enter Your Date of Birth","19-4-1992")
+// document.write(xyz)
+// var arr = ('hi','hello')
+
+// document.write(arr)
+
+// console.log(arr)
+
+// alert('user')
+// var NeelamFatimah; pascalcase
+// var $_xy = "neelam Fatimah";
+// var neelam_fatimah; snakecase
+
+// document.write(" my first name is");
+// document.write(" fatimah");
+
+// document.write(" my first name is");
+// document.write(" fatimah"); 
+// document.write(" my first name is");
+// document.write(" fatimah");
+// document.write(" my first name is");
+// document.write(" fatimah");
+// document.write(" my first name is");
+// document.write(" fatimah");
+// document.write(" my first name is");
+// document.write("fatimah");
+
+
+// document.write($_xy)
+// document.write(" my first name is");
+
+
+// document.write($_xy)
+// document.write(" my first name is");
+
+// document.write($_xy)
+// document.write(" my first name is");
+
+// document.write($_xy)
+// document.write(" my first name is");
+
+// document.write($_xy)
+// document.write(" my first name is");
+
+// document.write($_xy);
+
+// var hello="teacher";
+// var hello="usama";
+// document.write(hello);
+
+// var myage = 28
+// document.write(myage)
+
+
+// var firstname, lastname, age, identitynum, siblings, paymentmethod, sourceofincome; 
+// firstname = " neelam";
+// lastname =" fatimah";
+// age = "28";
+// identitynum ="4240124304";
+// sibling ="5 sisters 1bro death ";
+// paymentmethod ="hbl"
+// sourseofincome ="nothing jobless";
+
+// document.write("<br>my first name is");
+// document.write(firstname)
+
+// document.write("<br>my last name is");
+// document.write(lastname)
+
+// document.write( "<br>i m")
+// document.write(age)
+// document.write("years old")
+
+// // alert('enter your name please')
+
+// var firstname ="neelam"
+
+
+
+// var $_xy = "neelam Fatimah";
+// var neelam_fatimah; snakecase
+
+// document.write(" my first name is");
+// document.write(" fatimah");
+
+alert("thanks for your input")
+
+
+prompt("enter your CNIC 42401-751725-0")
+
+
+var xy= ('Ali')
+
+document.write("my first name is");
+document.write(" fatimah");
+document.write("<br>my first name is");
+document.write(" fatimah");
+document.write("<br>my first name is");
+document.write(" fatimah");
+document.write("<br>my first name is");
+document.write(" fatimah");
+document.write("<br>my first name is");
+document.write(" fatimah");
+document.write("<br>my first name is");
+document.write(" fatimah");
+
+document.write(xy);
+document.write("<br>my first name is");
+document.write(xy);
+document.write("<br>my first name is");
+document.write(xy);
+document.write("<br>my first name is")
+document.write(xy);
+
+
+var firstname, lastname, age, identitynum, siblings, paymentmethod, sourceofincome; 
+firstname = " neelam";
+lastname =" fatimah";
+age = "28";
+identitynum ="4240124304";
+sibling ="5 sisters 1bro death ";
+paymentmethod ="hbl"
+sourseofincome ="nothing jobless";
+
+document.write("<br>my first name is");
+document.write(firstname)
+
+document.write("<br>my last name is");
+document.write(lastname)
+
+document.write( "<br>i m")
+document.write(age)
+document.write("years old")
+
+// alert('enter your name please')
+
+// var firstname ="neelam"
+
+
+
+
+
+
+
